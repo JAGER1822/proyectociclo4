dd46e5d34c9caded1d6ed2d15181a33cce514983
javiergaleano <jgaleanoa@uninorte.edu.co>
2022-11-25 09:27:51 -0500

    commit de prueba

--- src/componentes/Cabecera.jsx
+++ src/componentes/Cabecera.jsx
@@ -1,5 +1,6 @@
 export function Cabecera () {
 
+   // comnentario contexto
     return (
         <div class="row">   
            <div class="p-4 mb-2 bg-primary text-white"> <h1> ServiRapidz </h1></div>
